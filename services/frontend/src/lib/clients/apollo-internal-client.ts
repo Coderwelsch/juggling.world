286fb4f0adec149438645f0f2a20797452d33855
Joseph Ribbe <social@coderwelsch.com>
2023-10-02 09:20:29 +0200

    added apollo clients for internal and browser/client usage

--- services/frontend/src/lib/clients/apollo-internal-client.ts
+++ services/frontend/src/lib/clients/apollo-internal-client.ts
@@ -1,10 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
-
-export const apolloClient = new ApolloClient({
+export const apolloInternalClient = new ApolloClient({
 	uri: process.env.INTERNAL_CMS_GRAPHQL_API_URL,
 	cache: new InMemoryCache(),
-	typeDefs: "",
 	headers: {
 		Authorization: `Bearer ${process.env.CMS_API_ACCESS_TOKEN}`,
 	},
