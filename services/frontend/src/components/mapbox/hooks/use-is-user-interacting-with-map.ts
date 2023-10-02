e025d6134f6664ee5cb4a0cbbfb395168dae7283
Joseph Ribbe <social@coderwelsch.com>
2023-10-02 09:22:10 +0200

    added use-is-user-interacting-with-map.ts

---
+++ services/frontend/src/components/mapbox/hooks/use-is-user-interacting-with-map.ts
@@ -0,0 +1,39 @@
+import mapboxgl from "mapbox-gl"
+import { useEffect } from "react"
+
+export const useIsUserInteractingWithMap = ({
+	map,
+	onInteractionStart,
+	onInteractionEnd,
+}: {
+	onInteractionStart: () => void
+	onInteractionEnd: () => void
+	map: mapboxgl.Map | undefined
+}) => {
+	useEffect(() => {
+		if (!map) {
+			return
+		}
+
+		const onMoveStart = () => {
+			onInteractionStart()
+		}
+
+		const onMoveEnd = () => {
+			onInteractionEnd()
+		}
+
+		map.on("mousedown", onMoveStart)
+		map.on("mouseup", onMoveEnd)
+
+		map.on("wheel", onMoveStart)
+		map.on("zoomstart", onMoveStart)
+
+		return () => {
+			map.off("mousedown", onMoveStart)
+			map.off("mouseup", onMoveEnd)
+
+			map.off("zoomstart", onMoveStart)
+		}
+	}, [map])
+}
