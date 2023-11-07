1fc326ac936cf6e6310854bd936d840f3fb5f364
Joseph Ribbe <social@coderwelsch.com>
2023-11-07 22:03:05 +0100

    increased padding

--- services/frontend/src/components/dashboard/components/panel/panel.tsx
+++ services/frontend/src/components/dashboard/components/panel/panel.tsx
@@ -9,7 +9,7 @@ export const Panel = ({ children, className }: PanelProps) => {
 	return (
 		<section
 			className={classNames(
-				"flex flex-col gap-6 rounded-xl border border-space-50/10 bg-space-100/20 p-6",
+				"flex flex-col gap-6 rounded-xl border border-space-50/10 bg-space-100/20 px-10 py-8",
 				className,
 			)}
 		>
