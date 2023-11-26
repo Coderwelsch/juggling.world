eb45ddf6be20bc363426230354b33e27cdc53fe2
Joseph Ribbe <social@coderwelsch.com>
2023-11-26 13:38:15 +0100

    added unauthenticated check/redirect

--- services/frontend/src/components/dashboard/hooks/use-user-session.ts
+++ services/frontend/src/components/dashboard/hooks/use-user-session.ts
@@ -1,8 +1,18 @@
 import { UserSessionData } from "@/pages/api/auth/[...nextauth]"
 import { useSession } from "next-auth/react"
+import { redirect } from "next/navigation"
 
 export const useUserSession = () => {
 	const session = useSession()
+
+	if (session.status === "loading") {
+		return null
+	}
+
+	if (session.status === "unauthenticated") {
+		// redirect to login
+		return redirect("/signin")
+	}
 
 	if (!session.data?.user) {
 		return null
