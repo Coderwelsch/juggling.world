df799c893cb40a267947416a92054e91b878a302
Joseph Ribbe <social@coderwelsch.com>
2023-11-26 13:42:27 +0100

    added display of selected filename

--- services/frontend/src/components/form/form-field/file-field/file-field.tsx
+++ services/frontend/src/components/form/form-field/file-field/file-field.tsx
@@ -1,7 +1,6 @@
-import { Button } from "@/src/components/button/button"
-import IconUpload from "@/src/components/icons/upload"
+import IconFileImage from "@/src/components/icons/file-image"
 import { classNames } from "@/src/lib/class-names"
-import React, { forwardRef } from "react"
+import React, { forwardRef, useEffect } from "react"
 
 interface FileInputFieldProps
 	extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -9,42 +8,66 @@ interface FileInputFieldProps
 	label?: string
 }
 
-// eslint-disable-next-line react/display-name
 export const FileField = forwardRef(
 	(
 		{
 			className,
 			id,
-			label = "Select …",
+			label = "Choose",
 			children,
+			onChange,
 			...props
 		}: FileInputFieldProps,
 		ref: React.ForwardedRef<HTMLInputElement>,
 	) => {
+		const [selectedFileName, setSelectedFileName] = React.useState<string>()
+
+		const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+			if (!e.target.files?.length) {
+				onChange?.(e)
+				return
+			}
+
+			setSelectedFileName(e.target.files[0].name)
+			onChange?.(e)
+		}
+
 		return (
 			<label
 				htmlFor={id}
 				className={classNames(
-					"rounded-full flex flex-row items-center bg-neutral-100 bg-opacity-10 placeholder-space-100 hover:bg-opacity-20 focus:bg-opacity-20" +
-						" transition-colors",
+					`group rounded-full flex flex-row items-center bg-neutral-100 bg-opacity-10 placeholder-violet-100 hover:bg-opacity-20 focus:bg-opacity-20 transition-colors cursor-pointer w-full`,
 					className,
 				)}
 			>
-				<Button
-					className={"rounded-r-none"}
-					IconBefore={<IconUpload />}
+				<span
+					className={
+						"flex flex-row items-center gap-2 rounded-l-full bg-violet-100/20 px-5 py-2 text-sm font-semibold text-neutral-50"
+					}
 				>
+					<IconFileImage />
 					{label}
-				</Button>
-
-				<span className={"px-5 text-sm text-violet-100/80"}>
-					No file selected
 				</span>
+
+				{selectedFileName ? (
+					<span
+						className={
+							"inline-block max-w-[12rem] overflow-hidden truncate px-5 text-sm text-violet-100/80"
+						}
+					>
+						{selectedFileName}
+					</span>
+				) : (
+					<span className={"px-5 text-sm text-violet-100/80"}>
+						No file selected
+					</span>
+				)}
 
 				<input
 					id={id}
 					ref={ref}
 					type={"file"}
+					onChange={handleChange}
 					hidden
 					{...props}
 				/>
