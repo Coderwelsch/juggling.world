2bced1ae1657eaa401189417b9e094507437a481
Joseph Ribbe <social@coderwelsch.com>
2023-11-26 13:43:21 +0100

    improved styling / added className support

--- services/frontend/src/components/dialog/dialog-body.tsx
+++ services/frontend/src/components/dialog/dialog-body.tsx
@@ -1,15 +1,18 @@
+import { classNames } from "@/src/lib/class-names"
 import React from "react"
 
 interface DialogBodyProps {
 	children: React.ReactNode
+	className?: string
 }
 
-export const DialogBody = ({ children }: DialogBodyProps) => {
+export const DialogBody = ({ children, className }: DialogBodyProps) => {
 	return (
 		<div
-			className={
-				"w-full max-w-2xl rounded-xl border border-space-50/20 bg-gray-800 px-5 py-4"
-			}
+			className={classNames(
+				"w-full max-w-2xl rounded-xl border border-space-50/20 bg-gray-800 px-5 py-4",
+				className,
+			)}
 		>
 			{children}
 		</div>
