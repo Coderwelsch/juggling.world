3ffca00a13d809ccf7fb02958b93f69467ee9754
Joseph Ribbe <social@coderwelsch.com>
2023-11-26 13:37:44 +0100

    added onMutate

--- services/frontend/src/hooks/data/user/use-update-avatar.ts
+++ services/frontend/src/hooks/data/user/use-update-avatar.ts
@@ -1,12 +1,15 @@
-import { useAuthorizedMutation } from "@/src/hooks/data/user/use-authorized-request"
+import { useAuthorizedMutation } from "@/src/hooks/data/user/use-authorized-mutation"
 import { UploadFile } from "@/src/types/cms/graphql"
 
-export const useUpdateAvatar = () => {
+export const useUpdateAvatar = (
+	props?: { onMutate?: () => void } | undefined,
+) => {
 	return useAuthorizedMutation<FormData, UploadFile>({
 		path: "/user/avatar",
 		invalidationKeys: ["/user/me"],
 		authOptions: {
 			method: "PUT",
 		},
+		onMutate: props?.onMutate,
 	})
 }
