f7dfa33139493254f3059708cdd82981360ad036
Joseph Ribbe <social@coderwelsch.com>
2023-11-26 13:45:41 +0100

    added auth check

--- services/frontend/pages/dashboard/index.tsx
+++ services/frontend/pages/dashboard/index.tsx
@@ -1,8 +1,28 @@
 import { SetupProfileSection } from "@/src/components/dashboard/components/setup-profile/setup-profile"
-import { useUserNeedsSetup } from "@/src/components/dashboard/components/setup-profile/setup-profile-dialog"
+
+import { useUserNeedsSetup } from "@/src/hooks/data/user/use-user-needs-setup"
 import { DashboardLayout } from "@/src/components/dashboard/layout"
 import { LoaderOverlay } from "@/src/components/loader-overlay/loader-overlay"
 import { UserProfileWrapper } from "@/src/hooks/data/user/use-profile-data"
+import { GetServerSideProps } from "next"
+import { getSession } from "next-auth/react"
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const session = await getSession(context)
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: "/signin",
+				permanent: false,
+			},
+		}
+	}
+
+	return {
+		props: {},
+	}
+}
 
 const Dashboard = () => {
 	const userNeedsSetup = useUserNeedsSetup()
