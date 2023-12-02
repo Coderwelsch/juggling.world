f5bd7fba39b6e2bf6fe94c16c058f71d955dd388
Joseph Ribbe <social@coderwelsch.com>
2023-12-02 23:07:10 +0100

    added toast notifications to mutations

--- services/frontend/src/hooks/data/auth/use-authorized-mutation.ts
+++ services/frontend/src/hooks/data/auth/use-authorized-mutation.ts
@@ -1,4 +1,5 @@
 import { useUserSession } from "@/src/components/dashboard/hooks/use-user-session"
+import { errorToast } from "@/src/components/toast/toast"
 import { NEXT_PUBLIC_CMS_API_URL } from "@/src/lib/constants"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 
@@ -7,30 +8,68 @@ export interface UseAuthorizedMutationProps {
 	invalidationKeys?: string[]
 	authOptions?: RequestInit
 	onMutate?: () => void
+	onError?: (error: Error) => void
+}
+
+export type ErrorResponse = {
+	error?: {
+		details: Record<string, unknown>
+		message: string
+		name: string
+		status: number
+	}
 }
 
 export const useAuthorizedMutation = <
 	MutationParams extends BodyInit,
-	ResponseType = unknown,
+	ResponseType extends ErrorResponse,
 >({
 	path,
 	invalidationKeys,
 	authOptions = {},
 	onMutate,
+	onError,
 }: UseAuthorizedMutationProps) => {
 	const user = useUserSession()
 	const queryClient = useQueryClient()
 
-	return useMutation<ResponseType, unknown, MutationParams>({
+	return useMutation<ResponseType, any, MutationParams>({
 		mutationKey: [path],
 		onMutate,
-		onSuccess: () => {
+		onSuccess: (response) => {
+			if (response.error) {
+				throw response.error
+			}
+
 			invalidationKeys?.forEach((key) => {
 				queryClient.invalidateQueries({
 					queryKey: [key],
 				})
 			})
 		},
+		onError:
+			onError ||
+			((error) => {
+				console.error(error)
+
+				if (error.status === 401) {
+					return errorToast({
+						message:
+							"You are not authorized to perform this action.",
+					})
+				}
+
+				if (error.status === 403) {
+					return errorToast({
+						message:
+							"You don't have permission to perform this action.",
+					})
+				}
+
+				return errorToast({
+					message: error.message,
+				})
+			}),
 		mutationFn: async (data: MutationParams) => {
 			if (!user) {
 				throw new Error("No user session")
