5c72b75b9b88460cb0d6f4c13e50eefc9667e5af
Joseph Ribbe <social@coderwelsch.com>
2023-12-03 15:16:14 +0100

    improved styling of create-a-group.tsx

--- services/frontend/src/components/dashboard/components/create-a-group/create-a-group.tsx
+++ services/frontend/src/components/dashboard/components/create-a-group/create-a-group.tsx
@@ -1,15 +1,33 @@
+import { Button } from "@/src/components/button/button"
 import { Panel } from "@/src/components/dashboard/components/panel/panel"
-import { Headline } from "@/src/components/headline/headline"
+import IconAddCircle from "@/src/components/icons/add-circle"
+import IconTeamwork from "@/src/components/icons/teamwork"
 
 export const CreateAGroup = () => {
 	return (
-		<Panel>
-			<Headline>Create your first group 🙏</Headline>
+		<Panel className={"relative"}>
+			<Panel.Header Icon={IconTeamwork}>Create a group</Panel.Header>
 
-			<p className={"opacity-60"}>
-				Create a juggling group for you and your friends to get notified
-				when someone is going to juggle.
-			</p>
+			<Panel.Body>
+				<p className={"opacity-60"}>
+					Create or join an already existing juggling group. Groups
+					are great ways for you and your friends to get notified when
+					someone is going to juggle.
+				</p>
+			</Panel.Body>
+
+			<Panel.Footer>
+				<Button
+					IconAfter={
+						<IconAddCircle
+							width={"100%"}
+							height={"100%"}
+						/>
+					}
+				>
+					Create a group
+				</Button>
+			</Panel.Footer>
 		</Panel>
 	)
 }
