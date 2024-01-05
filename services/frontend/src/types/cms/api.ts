7bcd111fbf345110b3d35ccb08965146b80ccd6e
Joseph Ribbe <social@coderwelsch.com>
2024-01-05 18:29:48 +0100

    updated types

--- services/frontend/src/types/cms/api.ts
+++ services/frontend/src/types/cms/api.ts
@@ -5,3 +5,52 @@ export enum LocationType {
 	BasketballCourt = "basketball-court",
 	Other = "other",
 }
+
+export interface Member {
+	id: number
+	username: string,
+	avatar?: {
+		id: number
+		url: string
+	}
+}
+
+export interface GroupEvent {
+	id: number
+	name: string
+	description: string
+	start: Date
+	admins: Array<Member>
+	location: {
+		id: number
+		name: "Bassinplatz"
+		type: LocationType
+		location: {
+			longitude: number
+			latitude: number
+		}
+		image?: {
+			id: number
+			url: string
+		}
+	}
+}
+
+export interface Group {
+	id: string
+	name: string
+	description?: string
+	isPrivate: boolean
+	isAdmin: boolean
+	members: Array<Member>
+	admins: Array<Member>
+	avatar?: {
+		url: string
+	}
+	location: {
+		latitude: number
+		longitude: number
+	}
+	events: GroupEvent[]
+}
+
