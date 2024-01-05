55bdb34d35e9f2e4da67caf9629526f80f3115f7
Joseph Ribbe <social@coderwelsch.com>
2024-01-05 18:32:46 +0100

    added select-location-field context

--- services/frontend/src/components/form/form-field/select-location-field/select-location-field.tsx
+++ services/frontend/src/components/form/form-field/select-location-field/select-location-field.tsx
@@ -1,29 +1,45 @@
 import { Map } from "@/src/components/mapbox/map"
 import { DotMarker } from "@/src/components/mapbox/marker/dot-marker"
+import { MarkerLabel } from "@/src/components/mapbox/marker/marker-label"
 import { classNames } from "@/src/lib/class-names"
 import mapboxgl from "mapbox-gl"
-import { ReactNode, useEffect, useRef, useState } from "react"
+import { createContext, ReactNode, useEffect, useRef, useState } from "react"
 
 export interface SelectLocationInputProps {
-	location?: mapboxgl.LngLat
+	location?: {
+		latitude: number
+		longitude: number
+	}
 	onChange: (location: mapboxgl.LngLat) => void
+	onMarkerClick?: (map: mapboxgl.Map | null) => void
 	className?: string
+	markerIntent?: Intent
+	markerLabel?: string
 	markerIcon?: ReactNode
+	children?: ReactNode
 }
 
-export const SelectLocationField = ({
-	location,
-	onChange,
-	className,
-	markerIcon,
-}: SelectLocationInputProps) => {
+export const SelectLocationFieldContext =
+	createContext<SelectLocationInputProps | null>(null)
+
+export const SelectLocationField = (props: SelectLocationInputProps) => {
+	const {
+		location,
+		onChange,
+		className,
+		markerIcon,
+		markerLabel,
+		markerIntent,
+		onMarkerClick,
+		children,
+	} = props
+
 	const [isMapLoaded, setIsMapLoaded] = useState(false)
 	const mapRef = useRef<mapboxgl.Map | null>(null)
 
-	const latitude = location?.lat ?? 12
-	const longitude = location?.lng ?? 52.5
+	const latitude = location?.latitude ?? 12
+	const longitude = location?.longitude ?? 52.5
 
-	// fly to current location
 	useEffect(() => {
 		if (!isMapLoaded) {
 			return
@@ -37,7 +53,7 @@ export const SelectLocationField = ({
 			return
 		}
 
-		// make shure to have the marker in the viewport
+		// make sure to have the marker in the viewport
 		mapRef.current.flyTo({
 			center: [longitude, latitude],
 			zoom: Math.max(7, mapRef.current.getZoom()),
@@ -62,15 +78,29 @@ export const SelectLocationField = ({
 					onChange(event.lngLat)
 				}}
 			>
-				{location && (
-					<DotMarker
-						location={[longitude, latitude]}
-						onClick={() => {}}
-						active={true}
-						focused={true}
-						icon={markerIcon}
-					/>
-				)}
+				<SelectLocationFieldContext.Provider value={props}>
+					{location && (
+						<DotMarker
+							location={[longitude, latitude]}
+							onClick={() =>
+								onMarkerClick && onMarkerClick(mapRef.current)
+							}
+							intent={markerIntent}
+							active={true}
+							focused={true}
+							icon={markerIcon}
+						>
+							{markerLabel && (
+								<MarkerLabel
+									label={markerLabel}
+									intent={"sun"}
+								/>
+							)}
+						</DotMarker>
+					)}
+
+					{children}
+				</SelectLocationFieldContext.Provider>
 			</Map>
 		</div>
 	)
