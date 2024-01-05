0fff7fb215b30ede0281e77279065570022ed80e
Joseph Ribbe <social@coderwelsch.com>
2024-01-05 18:24:26 +0100

    added NEXT_PUBLIC_META_APP_NAME

--- services/frontend/src/lib/constants.ts
+++ services/frontend/src/lib/constants.ts
@@ -1,11 +1,19 @@
 export const NODE_ENV = process.env.NODE_ENV || "development"
+
 export const NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN =
 	process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN || ""
+
 export const NEXT_PUBLIC_MAPBOX_STYLE_URL =
 	process.env.NEXT_PUBLIC_MAPBOX_STYLE_URL || ""
+
 export const NEXT_PUBLIC_CMS_GRAPHQL_API_URL =
 	process.env.NEXT_PUBLIC_CMS_GRAPHQL_API_URL || ""
+
 export const NEXT_PUBLIC_CMS_API_URL = process.env.NEXT_PUBLIC_CMS_API_URL || ""
+
+export const META_APP_NAME = process.env.NEXT_PUBLIC_META_APP_NAME || ""
+
 export const INTERNAL_CMS_GRAPHQL_API_URL =
 	process.env.INTERNAL_CMS_GRAPHQL_API_URL || ""
+
 export const CMS_API_ACCESS_TOKEN = process.env.CMS_API_ACCESS_TOKEN || ""
